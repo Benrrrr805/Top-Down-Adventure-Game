1e7cfd12086a11f228b366052ae14065b98d1a47
Benjamin <Benrrrr805@gmail.com>
2025-01-05 18:12:19 -0800

    fix: updated child classes to use correct name for image_url

--- game/scenes/uiComponents/buttons/button.py
+++ game/scenes/uiComponents/buttons/button.py
@@ -3,8 +3,8 @@ from game.settings import BLACK
 
 class Button(UIComponent):
     def __init__(self, name, width, height, x_coordinate, y_coordinate, 
-                 background_image=None, background_color = None, 
+                 image_url=None, background_color = None, 
                  text=None, text_font=None, text_size=24, text_color=BLACK, text_position=None):
         super().__init__(name, width, height, x_coordinate, y_coordinate,
-                         background_image, background_color,
+                         image_url, background_color,
                          text, text_font, text_size, text_color, text_position)
--- game/scenes/uiComponents/menus/menu.py
+++ game/scenes/uiComponents/menus/menu.py
@@ -2,9 +2,9 @@ from game.scenes.uiComponents.uiComponent import UIComponent
 from game.settings import BLACK
 class Menu(UIComponent):
     def __init__(self, name, width, height, x_coordinate, y_coordinate, 
-                 background_image=None, background_color=None, 
+                 image_url=None, background_color=None, 
                  text=None, text_font=None, text_size=24, text_color=BLACK, text_position=None):
         super().__init__(name, width, height, x_coordinate, y_coordinate, 
-                         background_image, background_color, 
+                         image_url, background_color, 
                          text, text_font, text_size, text_color, text_position)
         
