a489f09d4e2193834acf0ef7ee5d9833399977fe
Benjamin <Benrrrr805@gmail.com>
2025-01-19 23:39:02 -0600

    fix: updated tests

---
+++ run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PYTHONPATH=$(pwd)
+
+# echo "Running core tests"
+# pytest tests/core/ --cov=game.core --cov-report=term-missing -v
+
+# echo "Running validation tests"
+# pytest tests/validation/ --cov=game.validation --cov-report=term-missing -v
+
+# echo "Running scene tests"
+# pytest tests/scenes/ --cov=game.scenes --cov-report=term-missing -v
+
+echo "Running all tests together"
+pytest --cov=. --cov-report=term-missing -v
+
+echo "All tests completed!"
